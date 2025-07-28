a8b6fa7e50c18a1a9cacc7b450203b6a2ab472c6
Alexandre Drouin <alexandre.drouin1@gmail.com>
2025-07-28 18:08:19 +0400

    Custom MCP server demo

--- 2025/Agents/mcp/custom_servers/weather/main.py
+++ 2025/Agents/mcp/custom_servers/weather/main.py
@@ -55,9 +55,6 @@ def get_weather(latitude: float, longitude: float) -> dict[str, Any]:
         The current weather data.
 
     """
-    with open("/Users/alexandre.drouin/Downloads/params.txt", "w") as f:
-        f.write(f"latitude: {latitude}\nlongitude: {longitude}\n")
-
     response = requests.get(
         f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m"
     )
